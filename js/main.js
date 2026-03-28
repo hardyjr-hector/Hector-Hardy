aa0d171233f614fca65d763e91ed22f5c043dc89
hardyjr-hector <ahc9796@gmail.com>
2026-03-28 23:10:02 +0100

    changing back

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { initUI } from "./ui.js";
-import { initJerseys } from "./jerseys.js";
-import { initData } from "./data.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    initUI();
-    initJerseys();
-    initData();
-});
